33c11e3e97c1144cffa63bf9b4f920d7b50eca0e
Ivar Polman <ivarpolman@hotmail.com>
2021-04-22 13:28:23 +0200

    Built out profile

--- libs/content.php
+++ libs/content.php
@@ -1,3 +1,15 @@
 <?php
 
 $name = 'PP boy';
+
+$personInfo = [
+    'name' => 'Keesie',
+    'age' => 23,
+    'gender' => 'male',
+    'happy' => false,
+    'online' => true,
+    'favorites' => ['movie' => 'The Lion King',
+                    'club' => 'Barcelona',
+                    'color' => 'blue'
+                    ],
+                ];
